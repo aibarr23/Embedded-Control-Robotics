e63cc77525115bd416875d14362e03dd8abb0d17
Anthony Ibarra <aibarr23@uic.edu>
2025-10-10 11:24:19 -0500

    update to RTOS project folders, including an mini RTOS creation

--- RTOS/0/tm4c123-keil/main.c
+++ RTOS/0/tm4c123-keil/main.c
@@ -3,15 +3,38 @@
 
 
 
-/* background code: sequential with blocking version */
-int main(void) {
-    BSP_init();
+void main_blinky1() {
     while (1) {
         BSP_ledGreenOn();
         BSP_delay(BSP_TICKS_PER_SEC / 4U);
         BSP_ledGreenOff();
         BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
     }
+    
+}
+void main_blinky2() {
+    while (1) {
+        BSP_ledBlueOn();
+        BSP_delay(BSP_TICKS_PER_SEC / 4U);
+        BSP_ledBlueOff();
+        BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
+    }
+    
+}
 
-    //return 0;
+/* background code: sequential with blocking version */
+int main(){
+    uint32_t volatile run = 0U;
+
+    BSP_init();
+
+    if (run)
+    {
+        main_blinky1();
+    }
+    else{
+        main_blinky2();
+    }
+    
+    // return 0;
 }
