9cd8928db28e75a6b64998172e1efe18b0706539
Anthony Ibarra <aibarr23@uic.edu>
2025-10-23 12:36:26 -0500

    Embedded testing from quantum leaps educational videos

---
+++ Emb_Testing/Embedded-Test-main/examples/basic/src/sum.c
@@ -0,0 +1,6 @@
+// very simple code to test...
+#include "sum.h"
+
+int sum(int x, int y) {
+    return x + y;
+}
---
+++ Emb_Testing/Embedded-Test-main/examples/basic/src/sum.h
@@ -0,0 +1,7 @@
+// very simple code to test...
+#ifndef SUM_H_
+#define SUM_H_
+
+int sum(int x, int y);
+
+#endif // SUM_H_
