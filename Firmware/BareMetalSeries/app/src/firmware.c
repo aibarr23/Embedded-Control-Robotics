37364bad3ac87db0e4685f56296db15e89ec6268
Anthony Ibarra <aibarr23@uic.edu>
2025-11-02 22:45:48 -0600

    systick handler and init organization

--- Firmware/BareMetalSeries/app/src/firmware.c
+++ Firmware/BareMetalSeries/app/src/firmware.c
@@ -1,31 +1,31 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
-#define LED_PORT (GPIOA)
-#define LED_PIN  (LED_PIN)
+#include "core/system.h"
 
-static void rcc_setup(void){
-  rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
-}
+#define LED_PORT (GPIOA)
+#define LED_PIN  (GPIO5)
+
+#define CPU_FREQ      (84000000)
+#define SYSTICK_FREQ  (1000)
 
 static void gpio_setup(void){
   rcc_periph_clock_enable(RCC_GPIOA);
   gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
 }
 
-static void delay_cycles(uint32_t cucles){
-  for (uint32_t i = 0; i < cycles ; i++){
-    __asm__("nop");
-  }
-}
-
 int main(void) {
-  rcc_setup();
+  system_setup();
   gpio_setup();
 
+  uint64_t start_time = system_get_ticks();
+
   while(1){
-    gpio_toggle(LED_PORT, LED_PIN);
-    delay_cycles(84000000 / 4);
+    if(get_ticks() - start_time >= 1000){
+      gpio_toggle(LED_PORT, LED_PIN);
+      start_time = system_get_ticks();
+    }
+
   }
 
   // Never return
